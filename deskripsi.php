ef5b1c445be0381705391e1f3adb440299f5caeb
HandikaRonny <119285757+HandikaRonny@users.noreply.github.com>
2023-06-09 00:43:52 +0700

    Update deskripsi.php
    kata dashboard menjadi deskripsi

--- deskripsi.php
+++ deskripsi.php
@@ -103,9 +103,12 @@ $result_deskripsi = $conn->query($deskripsi);
         <div id="layoutSidenav_content">
             <main>
                 <div class="container-fluid px-4">
-                    <h1 class="mt-4">Dashboard</h1>
+                    <h1 class="mt-4">Deskripsi</h1>
                     <ol class="breadcrumb mb-4">
-                        <li class="breadcrumb-item active">Dashboard</li>
+                        <li class="breadcrumb-item">
+                            <a href="administrator.php">Dashboard</a>
+                        </li>
+                        <li class="breadcrumb-item active">Deskripsi</li>
                     </ol>
                     <div class="card mb-4">
                         <div class="card-header">
